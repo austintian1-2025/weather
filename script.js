aa29888d629f8e33fdabc4fcbf25813caa747229
austintian1-2025 <austin.tian1@gmail.com>
2025-12-10 10:47:02 -0500

    test

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const searchButton = document.getElementById('searchButton');
 const locationElement = document.getElementById('location');
 const temperatureElement = document.getElementById('temperature');
 const descriptionElement = document.getElementById('description');
+const weatherNowElement = document.getElementById('weathernow');
 
 searchButton.addEventListener('click', () => {
     const location = locationInput.value;
@@ -25,7 +26,13 @@ function fetchWeather(location) {
         .then(data => {
             locationElement.textContent = data.name;
             temperatureElement.textContent = `${Math.round(data.main.temp)}°C`;
-            descriptionElement.textContent = data.weather[0].description;
+            descriptionElement.textContent = data.weather[0].main;
+            try {
+            weatherNowElement.textContent = data.weather[1].main;
+                
+            } catch (error) {
+                
+            }
         })
         .catch(error => {
             console.error('Error fetching weather data:', error);
